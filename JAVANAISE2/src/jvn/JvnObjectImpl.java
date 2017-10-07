c92161680b3dd0a036dc25c3bef7c0e967d7e349
Anthony <chavoutier.anthony@gmail.com>
2017-10-07 15:25:24 +0200

    Revert "add enum LockState + duplicate client IRC2"
    This reverts commit 8bd16b8e31b2889848901d4a5758b3655a3166b7.

--- JAVANAISE2/src/jvn/JvnObjectImpl.java
+++ JAVANAISE2/src/jvn/JvnObjectImpl.java
@@ -4,40 +4,25 @@ import java.io.Serializable;
 
 public class JvnObjectImpl implements JvnObject {
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
 	private Serializable object = null;
-	
-	transient private LockState lock ;
-	
-	
-	private enum LockState {
-	    NL, // no local lock
-        RLC , // read lock cached
-        WLC , // write lock cached
-        RLT , // read lock taken
-        WLT , // write lock taken         
-        RLTWLC ; // read lock taken – write lock cached  
-	}
-	
-	
-	public JvnObjectImpl(Serializable object) throws JvnException {
+
+	public JvnObjectImpl(Serializable object) {
 		this.object = object;
-		jvnLockWrite(); // at the instantiation the object is locked in write lock mode
 	}
 	
 	public void jvnLockRead() throws JvnException {
 		// TODO Auto-generated method stub
+		
 	}
 
 	public void jvnLockWrite() throws JvnException {
 		// TODO Auto-generated method stub
+		
 	}
 
 	public void jvnUnLock() throws JvnException {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public int jvnGetObjectId() throws JvnException {
@@ -65,14 +50,4 @@ public class JvnObjectImpl implements JvnObject {
 		return null;
 	}
 
-	public LockState getLock() {
-		return lock;
-	}
-
-	public void setLock(LockState lock) {
-		this.lock = lock;
-	}
-	
-	
-	
 }
