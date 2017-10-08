0ec1828349bd3c4b276e06ca02d8366062f12d98
Mehdi Azzabi <mehdi.azzabi.08@gmail.com>
2017-10-08 17:28:21 +0200

    manage lock without coordinator interaction

--- JAVANAISE2/src/jvn/JvnObjectImpl.java
+++ JAVANAISE2/src/jvn/JvnObjectImpl.java
@@ -3,60 +3,85 @@ package jvn;
 import java.io.Serializable;
 
 public class JvnObjectImpl implements JvnObject {
-	
+
 	private Serializable object = null;
 	transient private LockState lock = LockState.NL;
-	
+
 	private enum LockState {
-		NL,    // no local lock
-		RLC,   // read lock cached
-		WLC,   // write lock cached
-		RLT,   // read lock taken
-		WLT,   // write lock taken
+		NL, // no local lock
+		RLC, // read lock cached
+		WLC, // write lock cached
+		RLT, // read lock taken
+		WLT, // write lock taken
 		RLTWLC; // read lock taken & write lock cached
 	}
 
 	public JvnObjectImpl(Serializable object) {
 		this.object = object;
+		this.setLock(LockState.NL);
 	}
-	
+
 	public void jvnLockRead() throws JvnException {
-		// TODO Auto-generated method stub
-		
+		synchronized (this.object) {
+			System.out.print("Operation LockRead : initial state : "+ this.lock + " " );
+			
+			if (this.lock == LockState.NL) {
+				// we will ask the coordinateur to take lock R and then this.setLock(LockState.RLT);
+				this.setLock(LockState.RLT);
+			} else if(this.lock == LockState.WLC) {
+				this.setLock(LockState.RLTWLC);
+			} else if(this.lock == LockState.RLC)
+				this.setLock(LockState.RLT);
+			//nous considérons que si l'objet est vérrouillé en écriture WLT
+			//nous gardons le verrous WLT car > RLT
+
+			System.out.println("passed to : "+ this.lock );
+		}
 	}
 
 	public void jvnLockWrite() throws JvnException {
 		synchronized (this.object) {
-			switch (this.lock) {
-				case NL:
-					this.setLock(LockState.WLT);
-					break;
-				case RLC:
-					
-					break;
-				case WLC:
-				
-					break;
-				case RLT:
-					
-					break;
-				case WLT:
-					
-					break;
-				case RLTWLC:
-					
-					break;
-				default:
-					break;
+
+			System.out.print("Operation LockWrite : initial state : "+ this.lock +" ");
+			if (this.lock == LockState.NL) {
+				// we will ask the coordinateur to take lock W and then this.setLock(LockState.WLT);
+				this.setLock(LockState.WLT);
+			} else {
+				this.setLock(LockState.WLT);
 			}
+			System.out.println("passed to : "+ this.lock );
 		}
-		
-		System.out.println(this.lock);
+
 	}
 
 	public void jvnUnLock() throws JvnException {
-		// TODO Auto-generated method stub
-		
+		synchronized (this.object) {
+
+			System.out.print("Unlock operation : initial lock :" + this.lock + " ");
+			switch (this.lock) {
+			case NL:
+				break;
+			case RLC:
+				this.setLock(LockState.NL);
+				break;
+			case WLC:
+				this.setLock(LockState.NL);
+				break;
+			case RLT:
+				this.setLock(LockState.RLC);
+				break;
+			case WLT:
+				this.setLock(LockState.WLC);
+				break;
+			case RLTWLC:
+				this.setLock(LockState.WLC);
+				break;
+			default:
+				break;
+			}
+		}
+
+		System.out.println(" passed to :" + this.lock);
 	}
 
 	public int jvnGetObjectId() throws JvnException {
@@ -71,26 +96,42 @@ public class JvnObjectImpl implements JvnObject {
 
 	public void jvnInvalidateReader() throws JvnException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public Serializable jvnInvalidateWriter() throws JvnException {
-		// TODO Auto-generated method stub
-		return null;
+		synchronized (this.object) {
+			while (this.lock == LockState.WLT) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			jvnUnLock();
+			return this.object;
+		}
 	}
 
 	public Serializable jvnInvalidateWriterForReader() throws JvnException {
-		// TODO Auto-generated method stub
-		return null;
+		synchronized (this.object) {
+			while (this.lock == LockState.WLT) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			return this.object;
+		}
 	}
-	
+
 	public LockState getLock() {
 		return this.lock;
 	}
-	
+
 	public JvnObjectImpl setLock(LockState lock) {
 		this.lock = lock;
-		
 		return this;
 	}
 }
